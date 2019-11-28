dc5a44e071abffd31d288fad69fd60f107584c72
Till Klocke <till.klocke@gmail.com>
2019-11-28 14:26:28 +0100

    Added utility method to created signed certificates

--- utils.go
+++ utils.go
@@ -18,6 +18,41 @@ func SignCertificate(cert *Certificate, priv ed25519.PrivateKey) (*Certificate, 
 	return cert, nil
 }
 
+// SignedCertificate creates a new certificate signed with the specified rooKey and issuer.
+func SignedCertificate(subject string, serialNumber uint64, notBefore, notAfter time.Time,
+	extensions []Extension, rootKey ed25519.PrivateKey, issuer string) (*Certificate, ed25519.PrivateKey, error) {
+
+	validity := &Validity{}
+	if extensions == nil {
+		extensions = []Extension{}
+	}
+	if notBefore.IsZero() {
+		validity.NotBefore = &ZeroTime
+	} else {
+		validity.NotBefore = NewTime(notBefore)
+	}
+	if notAfter.IsZero() {
+		validity.NotAfter = &ZeroTime
+	} else {
+		validity.NotAfter = NewTime(notAfter)
+	}
+	pub, priv, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, ed25519.PrivateKey{}, err
+	}
+	cert := &Certificate{
+		SerialNumber: serialNumber,
+		Issuer:       issuer,
+		Validity:     validity,
+		Subject:      subject,
+		PubKey:       pub,
+		Extensions:   extensions,
+		Signature:    nil,
+	}
+	cert, err = SignCertificate(cert, rootKey)
+	return cert, priv, err
+}
+
 // SelfSignedCertificate is a simple function to generate a self signed certificate
 func SelfSignedCertificate(subject string,
 	notBefore, notAfter time.Time,

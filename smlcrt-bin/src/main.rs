8b1a22dd8b31e92cea631c03bc580256a11be77f
Till Klocke <till.klocke@gmail.com>
2020-01-08 14:05:05 +0100

    First expirements with clap, looks primising so far

--- smlcrt-bin/src/main.rs
+++ smlcrt-bin/src/main.rs
@@ -1,3 +1,20 @@
+#[macro_use]
+extern crate clap;
+
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap_app!(myapp =>
+        (version: "0.1")
+        (author: "Till Klocke")
+        (about: "Creates, validates and manages smolcerts")
+        (@arg CONFIG: -c --config +takes_value "Sets a custom config file")
+        (@arg INPUT: +required "Sets the input file to use")
+        (@arg debug: -d ... "Sets the level of debugging information")
+        (@subcommand create =>
+            (about: "creates new certificates")
+            (@arg subject: -s --subject "The subject of the certificate")
+            (@arg serial_number: -sn --serial-number "The serial number of the certificate")
+        )
+    )
+    .get_matches();
 }

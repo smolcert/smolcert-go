164a477cc8d773dd8eea8363fea67ac22df00cd2
Till Klocke <till.klocke@gmail.com>
2020-01-13 11:28:07 +0100

    Switched away from macro based apprpach in clap since it is badly documented

--- smlcrt-bin/src/main.rs
+++ smlcrt-bin/src/main.rs
@@ -1,20 +1,46 @@
 #[macro_use]
 extern crate clap;
 
-
+use clap::{App, SubCommand, Arg};
 fn main() {
-    let matches = clap_app!(myapp =>
-        (version: "0.1")
-        (author: "Till Klocke")
-        (about: "Creates, validates and manages smolcerts")
-        (@arg CONFIG: -c --config +takes_value "Sets a custom config file")
-        (@arg INPUT: +required "Sets the input file to use")
-        (@arg debug: -d ... "Sets the level of debugging information")
-        (@subcommand create =>
-            (about: "creates new certificates")
-            (@arg subject: -s --subject "The subject of the certificate")
-            (@arg serial_number: -sn --serial-number "The serial number of the certificate")
-        )
-    )
-    .get_matches();
+    let matches = App::new("smlcrt")
+        .author("Till Klocke")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .subcommand(
+            SubCommand::with_name("create")
+            .args(&[
+                Arg::with_name("subject")
+                    .help("The subject of the new certificate")
+                    .short("s")
+                    .long("subject")
+                    .required(true)
+                    .takes_value(true),
+                Arg::with_name("serialnumber")
+                    .help("The serial number to assign to this certificate")
+                    .short("n")
+                    .long("serialnumber")
+                    .required(true)
+                    .takes_value(true)
+                    .validator(validate_serialnumber),
+            ])
+            )
+        .get_matches();
+
+    match matches.subcommand_name() {
+        Some("create") => create_certificate(),
+        _ => println!("Unknown subcommand"),
+    }
+}
+
+fn validate_serialnumber(v: String) -> Result<(), String> {
+    if !v.parse::<u64>().is_ok() {
+        Err(String::from("This is not a valid unsigned integer"))
+    } else {
+        Ok(())
+    }
+}
+
+fn create_certificate() {
+
 }
